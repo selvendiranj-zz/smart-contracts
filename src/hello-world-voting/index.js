7235df76d887b371efe9ec925f19bfbf2eed8d2d
selvendiranj <selvendiranj@gmail.com>
2017-09-05 20:51:34 +0530

    voting web app fixes

--- src/hello-world-voting/index.js
+++ src/hello-world-voting/index.js
@@ -1,39 +1,56 @@
+var web3;
+var abiContract;
+var votingContract;
+var contractAddress;
+var contractInstance;
+
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-
-var content;
-jQuery.getJSON('index.json', function(data) {
-    // data is an array of objects
-    content = data;
-    $.each(data, function() {
-        console.log(this.title); // log each title
-    });
-});
-
-
-var abi = JSON.parse(content);
-VotingContract = web3.eth.contract(abi);
-
-
-// In your nodejs console, execute contractInstance.address to 
-// get the address at which the contract is deployed and change
-// the line below to use your deployed address
-contractInstance = VotingContract.at('0x2a9c1d265d06d47e8f7b00ffa987c9185aecf672');
+contractAddress = '0x8ea087ea74f0957456b3ad8fa924d42059b2356c';
 candidates = { "Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3" }
 
-function voteForCandidate() {
-    candidateName = $("#candidate").val();
+var readContract = function() {
+
+    jQuery.getJSON('/index.json', function(data) {
+        // data is an array of objects
+        abiContract = data;
+        votingContract = web3.eth.contract(abiContract);
+        contractInstance = votingContract.at(contractAddress);
+    });
+
+}
+
+var getVotesForCandidate = function(candidateName) {
+    let div_id = candidates[candidateName];
+    let val = contractInstance.totalVotesFor.call(candidateName).toString();
+    $("#" + div_id).html(val);
+}
+
+/*
+In your nodejs console, execute contractInstance.address to 
+get the address at which the contract is deployed and change
+the line below to use your deployed address
+*/
+var voteForCandidate = function() {
+
+    var candidateName = $("#candidate").val();
     contractInstance.voteForCandidate(candidateName, { from: web3.eth.accounts[0] }, function() {
-        let div_id = candidates[candidateName];
-        $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
+        getVotesForCandidate(candidateName);
     });
 }
 
+var getVotesForAll = function() {
 
-$(document).ready(function() {
-    candidateNames = Object.keys(candidates);
+    var candidateNames = Object.keys(candidates);
+
     for (var i = 0; i < candidateNames.length; i++) {
         let name = candidateNames[i];
-        let val = contractInstance.totalVotesFor.call(name).toString()
-        $("#" + candidates[name]).html(val);
+        getVotesForCandidate(name);
     }
+}
+
+$(document).ready(function() {
+
+    readContract();
+    setTimeout(getVotesForAll, 5000);
+
 });
